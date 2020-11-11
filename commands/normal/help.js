30cb38186920a02782113315bd830c6ff23bf640
iscii <iscii@users.noreply.github.com>
2020-11-11 09:17:35 -0500

    Update help.js

--- commands/normal/help.js
+++ commands/normal/help.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "help",
     description: "help <[g]> | get list of commands (ex: |help // |help g)",
-    execute(msg, args, client, Discord) {
+    execute(msg, args, client, Discord, prefix) {
         let commands = "";
         let list = client.normCmds;
         if(args[0] == "g") list = client.gameCmds;
@@ -15,7 +15,7 @@ module.exports = {
         for(var key of list)
             commands += `- ${list.get(key[0]).description}\n`;
 
-        em.addField(`${args[0]=="g" ? "Game" : "Normal"} Commands`, commands);
+        em.addField(`${args[0]=="g" ? "Game Commands\nPrefix: <abbreviation>.\t(ex: ex.start)" : `Normal Commands\nPrefix: ${prefix}\t(ex: |help)`}`, commands);
         return msg.channel.send(em);
     },
 }
