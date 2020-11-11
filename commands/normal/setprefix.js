8761c28f1d4acdc3e4ae6e2f29536815e2041536
iscii <iscii@users.noreply.github.com>
2020-11-11 10:17:57 -0500

    two char prefixes

--- commands/normal/setprefix.js
+++ commands/normal/setprefix.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "setprefix",
     description: "setprefix <prefix> | sets the bot's prefix",
     execute(msg, admin, cmd, args, Discord) {
-        if(!/^[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/a-z0-9]{1}$/.test(args[0])) return msg.channel.send("Please limit the prefix to a single alphanumeric or symbol character");
+        if(!/^[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/a-z0-9]{2}$/.test(args[0])) return msg.channel.send("Please limit the prefix to a single alphanumeric or symbol character");
         admin.firestore().collection("guilds").doc(msg.guild.id).set({
             prefix: args[0]
         }).then(() => {
