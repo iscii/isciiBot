20058948dbda2dea438bd855b1890163fca6a661
iscii <issac.zheng9869@gmail.com>
2021-03-06 18:20:24 -0500

    reduced embed char count

--- commands/normal/help.js
+++ commands/normal/help.js
@@ -2,7 +2,6 @@ module.exports = {
     name: "help",
     description: "help [g] | get list of commands (ex: |help // |help g)",
     execute(msg, args, client, Discord, prefix) {
-        let commands = "";
         let list = client.normCmds;
         if(args[0] == "g") list = client.gameCmds;
         
@@ -11,11 +10,12 @@ module.exports = {
             .setThumbnail(msg.guild.iconURL())
             .setFooter("Hiii")
             .setTimestamp();
+        
+        em.addField("Info", `${args[0]=="g" ? "Game Commands\nPrefix: <abbreviation>.\t(ex: ex.start)" : `Normal Commands\nPrefix: ${prefix}\t(ex: ${prefix}help)`}`)
+        for(var key of list){
+            em.addField(key[0], "- "+list.get(key[0]).description);
+        }
 
-        for(var key of list)
-            commands += `- ${list.get(key[0]).description}\n`;
-
-        em.addField(`${args[0]=="g" ? "Game Commands\nPrefix: <abbreviation>.\t(ex: ex.start)" : `Normal Commands\nPrefix: ${prefix}\t(ex: |help)`}`, commands);
         return msg.channel.send(em);
     },
 }
