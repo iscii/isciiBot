7a181f1e2c2a1200c4911544783a736c45ec60cb
iscii <issac.zheng9869@gmail.com>
2021-03-13 21:55:03 -0500

    Update add.js

--- commands/normal/add.js
+++ commands/normal/add.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: "add",
-    description: "add <abbreviation> <name> | add a game to gamelist",
+    synopsis: "i.add <abbreviation> <name>",
+    description: "add a game to the gamelist",
+    options: "",
     async execute(msg, admin, cmd, args, Discord, client) {
         if (!args[1]) return msg.channel.send(this.description);
         if (/[^ ].{3}/.test(args[0])) return msg.channel.send("Please limit the abbreviation to three characters");
@@ -10,7 +12,7 @@ module.exports = {
         }
         await admin.firestore().collection("guilds").doc(msg.guild.id).set({
             gameList: {
-                [args[0]]: {
+                [`${args[0].toLowerCase()}`]: {
                     name: name,
                     color: "#000000",
                     description: "",
