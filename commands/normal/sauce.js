d33ca2df058b6b41b9d5a47959b5f7d40a1ef535
iscii <issac.zheng9869@gmail.com>
2021-08-14 19:10:20 -0400

    begin the sauce command

--- commands/normal/sauce.js
+++ commands/normal/sauce.js
@@ -4,6 +4,7 @@ module.exports = {
     description: "sauce <n>| google image search last <n> image attachments in channel and return the results",
     options: "",
     execute(msg, admin, cmd, args, Discord) {
+        const search = require("another-node-reverse-image-search");
         msg.channel.messages.fetch({limit: 20})
         .then(messages => {
             messages.forEach(message => {
@@ -11,5 +12,10 @@ module.exports = {
             });
             console.log(`recieved ${messages.size} messages`);
         })
+        //for each message, if it's an image, search and add results to an embed. format the embed and send the message, separately or together.
+        /* search("https://cdn.discordapp.com/attachments/494223131683061763/868615421362839583/91403647_p0_master1200.png", doSomething);
+        const doSomething = (results) => {
+
+        } */
     },
 }
