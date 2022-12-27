14d05ff9434e5e81d9d31a595b486d124539c28a
Issac <issac.zheng9869@gmail.com>
2022-12-27 12:36:42 -0500

    nto sure what fly is but these r format changes + committing for heroku autodeploy test

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ client = new Discord.Client();
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccount.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+	credential: admin.credential.cert(serviceAccount),
 });
-const db = admin.firestore(); 
+const db = admin.firestore();
 
 //fs
 const fs = require("fs");
@@ -19,147 +19,183 @@ client.gameCmds = new Discord.Collection();
 const utilities = require("./utilities.js");
 
 //login
-client.login(process.env.TOKEN); //-- use this if ever plan on making repo public
-//client.login(fs.readFileSync("./token.txt").toString().replace(/\s/g, ''));
+// client.login(process.env.TOKEN); //-- use this if ever plan on making repo public
+client.login(fs.readFileSync("./token.txt").toString().replace(/\s/g, ""));
 
 //get command files
-const normCmdFiles = fs.readdirSync("./commands/normal").filter(file => file.endsWith(".js"));
-const gameCmdFiles = fs.readdirSync("./commands/games").filter(file => file.endsWith(".js"));
+const normCmdFiles = fs
+	.readdirSync("./commands/normal")
+	.filter((file) => file.endsWith(".js"));
+const gameCmdFiles = fs
+	.readdirSync("./commands/games")
+	.filter((file) => file.endsWith(".js"));
 for (const file of normCmdFiles) {
-    if (file == "1-format.js") continue;
-    const command = require(`./commands/normal/${file}`);
-    client.normCmds.set(command.name, command);
+	if (file == "1-format.js") continue;
+	const command = require(`./commands/normal/${file}`);
+	client.normCmds.set(command.name, command);
 }
 for (const file of gameCmdFiles) {
-    if (file == "1-format.js") continue;
-    const command = require(`./commands/games/${file}`);
-    client.gameCmds.set(command.name, command);
+	if (file == "1-format.js") continue;
+	const command = require(`./commands/games/${file}`);
+	client.gameCmds.set(command.name, command);
 }
 
 //client
 client.on("guildCreate", (guild) => {
-    console.log(`Joined ${guild.name}`);
-    db.collection("guilds").doc(guild.id).set({
-        exists: true,
-        prefix: "i."
-    });
+	console.log(`Joined ${guild.name}`);
+	db.collection("guilds").doc(guild.id).set({
+		exists: true,
+		prefix: "i.",
+	});
 });
 
 client.on("guildDelete", (guild) => {
-    console.log(`Left ${guild.name}`);
-    db.collection("guilds").doc(guild.id).delete();
+	console.log(`Left ${guild.name}`);
+	db.collection("guilds").doc(guild.id).delete();
 });
 
 client.on("ready", async () => {
-    console.log("bot is ready");
-    connection = null;
-    /*
+	console.log("bot is ready");
+	connection = null;
+	/*
     var x = client.guilds.cache.array();
     for (i in x) {
         db.collection("guilds").doc(x[i].id).update({
 
         }, { merge: true });
     }*/
-    //time
-    let date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth() + 1; //getmonth starts from 0
-    let hour = date.getHours();
-    let till = 0;
+	//time
+	let date = new Date();
+	let day = date.getDate();
+	let month = date.getMonth() + 1; //getmonth starts from 0
+	let hour = date.getHours();
+	let till = 0;
 
-    if (hour < 6) {
-        till = 7 - hour;
-    }
-    else if (hour > 7) {
-        till = (24 - hour) + 7;
-        day++;
-    }
+	if (hour < 6) {
+		till = 7 - hour;
+	} else if (hour > 7) {
+		till = 24 - hour + 7;
+		day++;
+	}
 
-    let bd = await db.collection("birthdays").doc(month + "." + day).get();
-    let ppl = bd.data();
-    //console.log(till);
-    setTimeout(() => {
-        client.channels.fetch("745349500587212943").then(async (channel) => { //745349500587212943
-            //good morning
-            channel.send(`Good Morning!!! ${client.emojis.cache.find(emoji => emoji.name == "miyanohey")}`);
-            //birthdays
-            if (ppl) {
-                let users = "";
-                for (item in ppl.users) {
-                    users += `<@!${ppl.users[item]}> `;
-                }
-                channel.send(`Happy Birthday!!! :birthday: ${users}`);
-            }
-        });
-    }, till * 3600000);
+	let bd = await db
+		.collection("birthdays")
+		.doc(month + "." + day)
+		.get();
+	let ppl = bd.data();
+	//console.log(till);
+	setTimeout(() => {
+		client.channels.fetch("745349500587212943").then(async (channel) => {
+			//745349500587212943
+			//good morning
+			channel.send(
+				`Good Morning!!! ${client.emojis.cache.find(
+					(emoji) => emoji.name == "miyanohey"
+				)}`
+			);
+			//birthdays
+			if (ppl) {
+				let users = "";
+				for (item in ppl.users) {
+					users += `<@!${ppl.users[item]}> `;
+				}
+				channel.send(`Happy Birthday!!! :birthday: ${users}`);
+			}
+		});
+	}, till * 3600000);
 });
 
 client.on("message", async (msg) => {
-    if (msg.author.bot) return;
-    //holidays!! set a bot calendar thing that announces holidays/birthdays set by users
-    //replace game w event
-    //make bot autoping users in a queue
-    //tic tac toe and rps
-    //serveremotes list
-    //music
-    guild = db.collection("guilds").doc(msg.guild.id);
-    guildGet = await guild.get();
-    guildData = guildGet.data();
-    prefix = guildData.prefix;
+	if (msg.author.bot) return;
+	//holidays!! set a bot calendar thing that announces holidays/birthdays set by users
+	//replace game w event
+	//make bot autoping users in a queue
+	//tic tac toe and rps
+	//serveremotes list
+	//music
+	guild = db.collection("guilds").doc(msg.guild.id);
+	guildGet = await guild.get();
+	guildData = guildGet.data();
+	prefix = guildData.prefix;
 
-    if (msg.content.toLowerCase().startsWith(prefix)) cmdGeneral(msg);
-    else cmdGames(msg);
+	if (msg.content.toLowerCase().startsWith(prefix)) cmdGeneral(msg);
+	else cmdGames(msg);
 });
 
 async function cmdGeneral(msg) {
-    const cmd = msg.content.slice(prefix.length).trim().split(" ")[0];
-    const args = msg.content.slice(prefix.length).trim().split(" ").splice(1);
-    if (msg.content.includes(prefix)) {
-        //temporary for join and leave until you figure how to pass connection thru functions
-        if (cmd == "join") {
-            if (msg.member.voice.channel) connection = await msg.member.voice.channel.join(); else msg.channel.send("You are not in a voice channel");
-            return;
-        }
-        if (cmd == "leave" && connection) return connection.disconect();
-        if (client.normCmds.get(cmd) == undefined) return; //msg.channel.send("That command does not exist");
-        if (cmd == "help") return client.normCmds.get("help").execute(msg, args, client, Discord, prefix);
+	const cmd = msg.content.slice(prefix.length).trim().split(" ")[0];
+	const args = msg.content.slice(prefix.length).trim().split(" ").splice(1);
+	if (msg.content.includes(prefix)) {
+		//temporary for join and leave until you figure how to pass connection thru functions
+		if (cmd == "join") {
+			if (msg.member.voice.channel)
+				connection = await msg.member.voice.channel.join();
+			else msg.channel.send("You are not in a voice channel");
+			return;
+		}
+		if (cmd == "leave" && connection) return connection.disconect();
+		if (client.normCmds.get(cmd) == undefined) return; //msg.channel.send("That command does not exist");
+		if (cmd == "help")
+			return client.normCmds
+				.get("help")
+				.execute(msg, args, client, Discord, prefix);
 
-        console.log(`${msg.author} ${cmd} ${args}`);
+		console.log(`${msg.author} ${cmd} ${args}`);
 
-        try {
-            client.normCmds.get(cmd).execute(msg, admin, cmd, args, Discord, client);
-        }
-        catch (error) {
-            msg.channel.send("There was an error");
-            console.log(error);
-        }
-    }
+		try {
+			client.normCmds
+				.get(cmd)
+				.execute(msg, admin, cmd, args, Discord, client);
+		} catch (error) {
+			msg.channel.send("There was an error");
+			console.log(error);
+		}
+	}
 }
 async function cmdGames(msg) {
-    const game = msg.content.toLowerCase().trim().split(" ")[0].split(".")[0];
-    const cmd = msg.content.trim().split(" ")[0].split(".")[1];
-    const args = msg.content.trim().split(" ").splice(1);
+	const game = msg.content.toLowerCase().trim().split(" ")[0].split(".")[0];
+	const cmd = msg.content.trim().split(" ")[0].split(".")[1];
+	const args = msg.content.trim().split(" ").splice(1);
 
-    gameList = guildData.gameList;
-    embedChannel = guildData.embedChannel;
+	gameList = guildData.gameList;
+	embedChannel = guildData.embedChannel;
 
-    if (msg.content.includes(".") && (gameList != undefined && gameList.hasOwnProperty(game))) {
-        if (client.gameCmds.get(cmd) == undefined) return; //msg.channel.send("That command does not exist");
-        if (cmd == "help") return client.normCmds.get("help").execute(msg, args, client, Discord, prefix);
+	if (
+		msg.content.includes(".") &&
+		gameList != undefined &&
+		gameList.hasOwnProperty(game)
+	) {
+		if (client.gameCmds.get(cmd) == undefined) return; //msg.channel.send("That command does not exist");
+		if (cmd == "help")
+			return client.normCmds
+				.get("help")
+				.execute(msg, args, client, Discord, prefix);
 
-        console.log(`${msg.author} ${game} ${cmd} ${args}`);
+		console.log(`${msg.author} ${game} ${cmd} ${args}`);
 
-        session = guild.collection("sessions").doc(game);
-        sessionGet = await session.get();
+		session = guild.collection("sessions").doc(game);
+		sessionGet = await session.get();
 
-        try {
-            client.gameCmds.get(cmd).execute(msg, session, sessionGet, gameList, embedChannel, game, args, client, admin, Discord);
-        }
-        catch (error) {
-            msg.channel.send("There was an error");
-            console.log(error);
-        }
-    }
+		try {
+			client.gameCmds
+				.get(cmd)
+				.execute(
+					msg,
+					session,
+					sessionGet,
+					gameList,
+					embedChannel,
+					game,
+					args,
+					client,
+					admin,
+					Discord
+				);
+		} catch (error) {
+			msg.channel.send("There was an error");
+			console.log(error);
+		}
+	}
 }
 
 //use webhooks
